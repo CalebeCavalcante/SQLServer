3377ec8295d275873521d549da18093c1ef7b165
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2017-05-03 10:00:31 -0300

    Update Table_Alter_Table_Temp.sql

--- Table_Alter_Table_Temp.sql
+++ Table_Alter_Table_Temp.sql
@@ -7,21 +7,21 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-/****** Declarar uma vari·vel TABLE para guardar os dados atuais ******/
-/****** Manter os mesmos formatos - Dica: Ir na Tabela -> Bot„o Direito > Scripts -> Create Table :  Gera o script de criaÁ„o que o Management Studio disponibiliza ******/
+/****** Declarar uma vari√°vel TABLE para guardar os dados atuais ******/
+/****** Manter os mesmos formatos - Dica: Ir na Tabela -> Bot√£o Direito > Scripts -> Create Table :  Gera o script de cria√ß√£o que o Management Studio disponibiliza ******/
 /****** Copiar os campos e formatos e colocar aqui ******/
 DECLARE @TABLE_TEMP TABLE  (
 	[MATRICULA] [int] NOT NULL,
 	[NOME_COMPLETO] [nvarchar](255) NOT NULL
 ) 
 
-/****** Inserindo dados na tabela tempor·ria ******/
+/****** Inserindo dados na tabela tempor√°ria ******/
 INSERT @TABLE_TEMP SELECT * FROM [dbo].[TB_USUARIOS]
 
 /****** Deletando Objeto Table ******/
 DROP TABLE  [dbo].[TB_USUARIOS]
 
-/****** Criando a Tabela Novamente Com os Novos campos necess·rios ******/
+/****** Criando a Tabela Novamente Com os Novos campos necess√°rios ******/
 CREATE TABLE [dbo].[TB_USUARIOS](
 	[MATRICULA] [int] NOT NULL,
 	[NOME_COMPLETO] [nvarchar](255) NOT NULL
@@ -30,15 +30,18 @@ CREATE TABLE [dbo].[TB_USUARIOS](
 	[STATUS_CODE] [int] NOT NULL
 ) ON [PRIMARY]
 
-/****** Inserindo dados da Table Tempor·rio para manter o histÛrico ******/
-/****** ObservaÁıes:
+/****** Inserindo dados da Table Tempor√°rio para manter o hist√≥rico ******/
+/****** Observa√ß√µes:
 
-… obrigatÛrio listar os campos que ser„o inseridos, pois colocar um insert into table select * from n„o vai funcionar pois novos campos foram inseridos na criaÁ„o da tabela.
-O comando "GO" n„o pode ser executado antes de inserir, pois se for feito, os dados tempor·rios ser„o apagados
+√â obrigat√≥rio listar os campos que ser√£o inseridos, pois colocar um insert into table select * from n√£o vai funcionar pois novos campos foram inseridos na cria√ß√£o da tabela.
+O comando "GO" n√£o pode ser executado antes de inserir, pois se for feito, os dados tempor√°rios ser√£o apagados
  ******/
 INSERT [dbo].[TB_USUARIOS] (
 	[MATRICULA],
 	[NOME_COMPLETO] 
-) SELECT * FROM @TABLE_TEMP
+) SELECT 
+	[MATRICULA],
+	[NOME_COMPLETO]
+FROM @TABLE_TEMP
 
 GO
